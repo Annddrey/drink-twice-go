faaecc72aa2757322aae69b750de76e4a644dde7
Andrey Mescherion <androidmesch@mail.ru>
2024-08-13 15:29:01 +0300

    header mobail

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,1 +1,11 @@
-/* в этот файл добавляет скрипты*/
+const nav = document.querySelector(".nav");
+const navButton = document.querySelector(".nav__nav-button");
+
+if (nav) {
+  nav.classList.remove("nav--nojs");
+
+  navButton.addEventListener("click", () => {
+    nav.classList.toggle("nav--closed");
+    nav.classList.toggle("nav--opened");
+  });
+}
