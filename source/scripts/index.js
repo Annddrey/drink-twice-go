c0a7946473ec84d8b197536bade9dc9138e17d6d
Andrey Mescherion <androidmesch@mail.ru>
2024-08-14 17:19:28 +0300

    promo

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,11 +1,23 @@
-const nav = document.querySelector(".nav");
-const navButton = document.querySelector(".nav__nav-button");
+import Swiper from 'swiper/bundle';
+
+const nav = document.querySelector('.nav');
+const navButton = document.querySelector('.nav__nav-button');
 
 if (nav) {
-  nav.classList.remove("nav--nojs");
+  nav.classList.remove('nav--nojs');
 
-  navButton.addEventListener("click", () => {
-    nav.classList.toggle("nav--closed");
-    nav.classList.toggle("nav--opened");
+  navButton.addEventListener('click', () => {
+    nav.classList.toggle('nav--closed');
+    nav.classList.toggle('nav--opened');
   });
 }
+
+const swiper = new Swiper('.product-swiper', {
+  slidesPerView: 1,
+  loop: true,
+  spaceBetween: 40,
+  navigation: {
+    prevEl: '.product-swiper__button--prev',
+    nextEl: '.product-swiper__button--next',
+  },
+});
