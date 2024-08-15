de642252d16732f62315f41077ca98471efd0a57
Andrey Mescherion <androidmesch@mail.ru>
2024-08-15 12:56:24 +0300

    catalog - product-filter

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,5 +1,16 @@
 import Swiper from 'swiper/bundle';
 
+const productFilterSlider = document.querySelector('.product-filter__slider');
+
+noUiSlider.create(productFilterSlider, {
+  range: {
+    'min': 0,
+    'max': 100
+  },
+  start: [20, 80],
+  connect: true,
+});
+
 const nav = document.querySelector('.nav');
 const navButton = document.querySelector('.nav__nav-button');
 
