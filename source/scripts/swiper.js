94229b2f276a97c8d28f1687f861c84357e13d7c
Andrey Mescherion <androidmesch@mail.ru>
2024-08-18 19:50:50 +0300

    фоны слайдов

--- source/scripts/swiper.js
+++ source/scripts/swiper.js
@@ -3,7 +3,6 @@ import Swiper from 'swiper/bundle';
 const swiper = new Swiper('.product-swiper', {
   slidesPerView: 1,
   loop: true,
-  spaceBetween: 50,
   navigation: {
     prevEl: '.product-swiper__button--prev',
     nextEl: '.product-swiper__button--next',
